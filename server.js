bc533e17b3e1e96510dba6872efece912b6e358a
ccashley12 <ccashley12@gmail.com>
2024-03-09 14:54:54 -0500

    added code for URL and fs functions

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
-const http = require('http');
+const http = require('http'),
+    fs = require('fs'),
+    url = require('url');
 
 http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node!\n');
+    let addr = request.url,
+        q = new URL(addr, 'http://localhost:8080' + request.headers.host),
+        filePath = '';
+
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+
+    if (q.pathname.includes('documentation')) {
+        filePath = (_dirname + '/documentation.html');
+    } else {
+        filePath + 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end('Hello Node!\n');
+    });
+
 }).listen(8080);
 
 console.log('My first Node test server is running on port 8080.');
