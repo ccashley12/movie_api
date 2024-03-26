9fe2e65e348a31a2a666aec5880b210414c558b7
ccashley12 <ccashley12@gmail.com>
2024-03-26 12:58:38 -0400

    added needed code

--- index.js
+++ index.js
@@ -69,5 +69,5 @@ app.get('/movies', (req, res) => {
 
 //listen for requests
 app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.')
+    console.log('Your app is listening on port 8080.');
 });
