599b7cf3a127e319e90f977ec0e0dbb099bc7ec7
ccashley12 <ccashley12@gmail.com>
2024-03-26 12:45:44 -0400

    added GET route

--- index.js
+++ index.js
@@ -1,2 +1,73 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+    {
+        title: 'Perks of Being a Wallflower 2012',
+        director: 'Stephen Chbosky',
+        writer: 'Stephen Chbosky'
+    },
+    {
+        title: 'Poor Things 2023',
+        director: 'Yorgos Lanthimos',
+        writers: 'Tony McNamara & Alasdair Gray'
+    },
+    {
+        title: 'Everything Everywhere All at Once 2022',
+        director: 'Daniel Kwan & Daniel Scheinert',
+        writer: 'Daniel Kwan & Daniel Scheinert'
+    },
+    {
+        title: 'Parasite 2019',
+        director: 'Bong Joon Ho',
+        writer: 'Bong Joon Ho & Han Jin-won'
+    },
+    {
+        title: 'Pulp Fiction 1994',
+        director: 'Quentin Tarantino',
+        writer: 'Quentin Tarantino & Roger Avary'
+    },
+    {
+        title: 'Forrest Gump 1994',
+        director: 'Robert Zemeckis',
+        writer: 'Winston Groom & Eric Roth'
+    },
+    {
+        title: 'The Matrix 1999',
+        director: 'Lana Wachowski & Lilly Wachowski',
+        writer: 'Lana Wachowski & Lilly Wachowski'
+    },
+    {
+        title: 'Charlie\'s Angels 2000',
+        director: 'McG',
+        writer: 'Ryan Rowe, Ed Solomon, & John August'
+    },
+    {
+        title: 'American Psycho',
+        director: 'Mary Harron',
+        writer: 'Bret Easton Ellis, Mary Harron, & Guinevere Turner'
+    },
+    {
+        title: 'Donnie Darko',
+        director: 'Rirchard Kelly',
+        writer: 'Richard Kelly'
+    }
+];
+
+//GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to Cinema Express!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+//listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.')
+});
