b3737f2a487aeb2920c5acbec48efb383b98b233
ccashley12 <ccashley12@gmail.com>
2024-07-09 14:27:31 -0400

    added code for bcrypt

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const bcrypt =  require('bcrypt');
+
 let movieSchema = mongoose.Schema(
     {
         Title: {type: String, required: true},
@@ -25,6 +27,14 @@ let userSchema = mongoose.Schema(
         Birthday: Date,
         FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 
 let directorSchema = mongoose.Schema(
     {
